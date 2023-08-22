f8d37bd02b6e7f671e2c8b0d720ae657ff11dc83
TARUN DALBERA <138133429+Tarun45GitHub@users.noreply.github.com>
2023-08-22 06:36:23 +0000

    first

---
+++ 01_Variable.js
@@ -0,0 +1,31 @@
+const accountId=21101106045
+let accountName="Tarun";
+var accountEmail="tarun@gmail.com"
+accountState="west bengal"
+
+// console.log(accountId);
+// console.log(accountName);
+// console.log(accountEmail);
+// console.log(accountState);
+
+// accountId=2110204578 //re-assign not possible
+// console.log(accountId);
+
+// accountName="Dalbera"
+// console.log(accountName);
+
+// accountEmail="tk@gmail.com"
+// console.log(accountEmail);
+
+// accountState="kolkata"
+// console.log(accountState);
+
+console.table([accountId,accountName,accountEmail,accountState])//shortcurt 
+
+let accountNumber
+console.log(accountNumber); //undefined
+
+/*
+notes-
+use const for constantly assigntly value
+prefer not use var ,because issu in block scope and functional scope */
