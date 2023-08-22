a27f426b7d8d9a309dae246221979ee669d47eb0
TARUN DALBERA <138133429+Tarun45GitHub@users.noreply.github.com>
2023-08-22 07:24:50 +0000

    DataType

---
+++ 02_DataType.js
@@ -0,0 +1,29 @@
+"use strict" //treat all Js code as newer version
+
+//alert("ok")  //it run in browser not in nodejs
+ 
+console.log(3+
+    3); console.log("tarun"); //bad practice
+//code readability is should be high
+
+let number=353
+let name="tarun";
+let isLogged=true
+
+/*different datatype
+number=> range2^53
+string =>""
+boolean=> true/false
+null=> standalone value
+undefined=> value not defined
+symbol=>
+
+object =>special type
+*/
+console.table([typeof number,typeof name,typeof isLogged]) //typeof use define type of datatype
+console.log(typeof null); //object type
+console.log(typeof undefined); //undefined type
+console.log(typeof Symbol); //function type 
+console.log(typeof Object); //function type
+
+
