4298c7cd48978b7b9ccf4b2be0e2a6969eeacf3b
TARUN DALBERA <138133429+Tarun45GitHub@users.noreply.github.com>
2023-08-23 06:50:35 +0000

    operator

---
+++ 03_Operations.js
@@ -0,0 +1,11 @@
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+"2"+3); //123
+console.log(1+2+"3");//33
+
+let gamecounter=100;
+console.log(gamecounter);
+
+console.log(gamecounter++);
+
+console.log(++gamecounter);
