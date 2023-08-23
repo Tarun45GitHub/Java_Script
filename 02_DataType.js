2abf3c1bbe243f247211addd58e096087e52192a
TARUN DALBERA <138133429+Tarun45GitHub@users.noreply.github.com>
2023-08-23 06:33:49 +0000

    datatype conversion

--- 02_DataType.js
+++ 02_DataType.js
@@ -2,8 +2,8 @@
 
 //alert("ok")  //it run in browser not in nodejs
  
-console.log(3+
-    3); console.log("tarun"); //bad practice
+// console.log(3+
+//     3); console.log("tarun"); //bad practice
 //code readability is should be high
 
 let number=353
@@ -20,10 +20,24 @@ symbol=>
 
 object =>special type
 */
-console.table([typeof number,typeof name,typeof isLogged]) //typeof use define type of datatype
-console.log(typeof null); //object type
-console.log(typeof undefined); //undefined type
-console.log(typeof Symbol); //function type 
-console.log(typeof Object); //function type
+// console.table([typeof number,typeof name,typeof isLogged]) //typeof use define type of datatype
+// console.log(typeof null); //object type
+// console.log(typeof undefined); //undefined type
+// console.log(typeof Symbol); //function type 
+// console.log(typeof Object); //function type 
+
+ /************************************************************************************************ */
+ let num1="33a";
+ console.log( typeof num1);
+ let num2=Number(num1);
+ console.log( typeof num2);
+ console.log(num2);
+ //"33"(string)=>33(number)
+ //"33q"(string)=>NaN(number)
+ //true/false(boolean)=>1/0(number)
+ //also can convert number to string
+
+
+
 
 
