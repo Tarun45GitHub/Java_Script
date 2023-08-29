0bd4e52f1c5bc2abea848a8869784554167b0672
TARUN DALBERA <138133429+Tarun45GitHub@users.noreply.github.com>
2023-08-29 18:37:29 +0000

    L-12

---
+++ 06_NumberAndMath.js
@@ -0,0 +1,16 @@
+//#######################Number#####################################
+const score=200
+console.log(score);
+
+const number=new Number(300)
+console.log(number);
+console.log(number.toFixed(2));
+//#############################################Math#####################
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.random()); //random value between one to zero
+
+const min=10
+const max=20
+console.log(Math.floor((Math.random()*(max-min+1))+min));
