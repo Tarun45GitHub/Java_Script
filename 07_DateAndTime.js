211e97194418ca8ca472849a5166113510b19640
TARUN DALBERA <138133429+Tarun45GitHub@users.noreply.github.com>
2023-08-29 18:53:43 +0000

    L-13

---
+++ 07_DateAndTime.js
@@ -0,0 +1,7 @@
+let D1=new Date();
+console.log(D1);
+console.log(D1.toDateString());
+console.log(D1.toLocaleTimeString());
+console.log(typeof D1);
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
