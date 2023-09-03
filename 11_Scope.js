96a58f4f63b0667eec4f9f7b087622d4094ba5f1
TARUN DALBERA <138133429+Tarun45GitHub@users.noreply.github.com>
2023-09-03 05:59:20 +0000

    L-22

--- 11_Scope.js
+++ 11_Scope.js
@@ -20,3 +20,28 @@ let a=30
  }
  console.log(a);
  */
+/*
+function one(){
+    const fname="Tarun"
+    function two (){
+        const lname="Dalber"
+        console.log(fname);
+    }
+   // console.log(lname);
+    two()
+}
+one()
+*/
+/*
+console.log(addone(5)); //This type function can acess before declaration
+ function addone(num){
+    return num+1;
+ }
+ console.log(addone(5));
+
+//  console.log(addtwo(5)); This type function cann't access before declaration
+ const addtwo=function(num){
+    return num+2;
+ }
+ console.log(addtwo(5));
+ */
