4552e07c91f3946bc34734206fa113772f64745b
TARUN DALBERA <138133429+Tarun45GitHub@users.noreply.github.com>
2023-09-03 05:15:03 +0000

    L-21

---
+++ 11_Scope.js
@@ -0,0 +1,22 @@
+ /*if(true){
+    let a=10
+    const b=20
+    var c=30
+    d=40
+ }
+ //console.log(a);
+ //console.log(b);
+ console.log(c);
+ console.log(d);
+ */
+/*
+let a=30
+ if(true){
+    let a=10
+    const b=20
+    var c=30
+    d=40
+    console.log("inner a",a);
+ }
+ console.log(a);
+ */
