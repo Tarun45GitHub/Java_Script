ac6c66e1fbd970995dfc79f287cd94c122c48fef
Tarun45GitHub <td2545@it.jgec.ac.in>
2024-01-30 20:31:39 +0530

    variable second time

--- 01Variable/01_Variable.js
+++ 01Variable/01_Variable.js
@@ -1,24 +1,22 @@
-const accountId=21101106045
+const accountId=21101106045;
+console.log(accountId);
+// accountId=2110204578; //re-assign not possible for const type variable
+
 let accountName="Tarun";
-var accountEmail="tarun@gmail.com"
-accountState="west bengal"
+console.log(accountName);
+accountName="Dalbera";
+console.log(accountName);
 
-// console.log(accountId);
-// console.log(accountName);
-// console.log(accountEmail);
-// console.log(accountState);
+var accountEmail="tarun@gmail.com";
+console.log(accountEmail);
+accountEmail="tk@gmail.com";
+console.log(accountEmail);
 
-// accountId=2110204578 //re-assign not possible
-// console.log(accountId);
-
-// accountName="Dalbera"
-// console.log(accountName);
-
-// accountEmail="tk@gmail.com"
-// console.log(accountEmail);
-
-// accountState="kolkata"
-// console.log(accountState);
+accountState="west bengal";
+console.log(accountState);
+accountState=8520;
+console.log(accountState);
+console.log(typeof(accountState));
 
 console.table([accountId,accountName,accountEmail,accountState])//shortcurt 
 
@@ -27,5 +25,5 @@ console.log(accountNumber); //undefined
 
 /*
 notes-
-use const for constantly assigntly value
-prefer not use var ,because issu in block scope and functional scope */
+use const for constantly assigntly value.
+prefer not use var -> because issu in block scope and functional scope({}) */
